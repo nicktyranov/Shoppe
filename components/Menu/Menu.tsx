dc0eaee81e40ab695ca79a5d28cf25ee78429edd
Николай <nick@MacBook-Pro-Nikolaj.local>
2024-06-24 18:55:13 -0600

    mobile menu

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -6,11 +6,16 @@ import logo from './logo.svg';
 import likeIcon from './like-icon.svg';
 import userIcon from './user-icon.svg';
 import mobileMenuIcon from './mobileMenu.svg';
+import logoutIcon from './logout-icon.svg';
 import Search from '../Search/Search';
 import Link from 'next/link';
 import Cart from '../Cart/Cart';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
 
 export default function Menu({}: IMenuProps) {
+   const [isMobileMenuOpened, setIsMobileMenuOpened] = useState(false);
+   const isLogined = true;
    return (
       <div className={styles.wrapper}>
          <div className={styles.menu}>
@@ -47,8 +52,45 @@ export default function Menu({}: IMenuProps) {
                src={mobileMenuIcon}
                alt="mobile menu icon"
                id="mobileMenuIcon"
+               onClick={() => setIsMobileMenuOpened(!isMobileMenuOpened)}
             />
             <Search isClicked={true} className={styles['mobile-search']} />
+            {isMobileMenuOpened && (
+               <motion.div
+                  className={styles['mobile-navigation-wrapper']}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5 }}
+               >
+                  <nav className={styles['mobile-navigation']}>
+                     <Link href={'/shop'}>Main</Link>
+                     <Link href={'/shop'}>Shop</Link>
+                     <Link href={'/about'}>About</Link>
+                     <hr />
+                     <Link href={'/user'} className={styles['link-with-icon']}>
+                        <Image src={userIcon} alt="user icon" />
+                        <p>My account</p>
+                     </Link>
+                     <Link
+                        href={'/favorites'}
+                        className={styles['link-with-icon']}
+                     >
+                        <Image src={likeIcon} alt="favorites icon" />
+                        <p>My favorites</p>
+                     </Link>
+
+                     {isLogined && (
+                        <Link
+                           href={'/logout'}
+                           className={styles['link-with-icon']}
+                        >
+                           <Image src={logoutIcon} alt="logout icon" />
+                           <p>Logout</p>
+                        </Link>
+                     )}
+                  </nav>
+               </motion.div>
+            )}
          </div>
       </div>
    );
