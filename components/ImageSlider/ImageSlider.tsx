7077f93f1a4ab4df0bf0ce1a9051998fcab0c92a
Николай <nick@MacBook-Pro-Nikolaj.local>
2024-07-22 17:14:50 -0600

    code-review - fixed

--- components/ImageSlider/ImageSlider.tsx
+++ components/ImageSlider/ImageSlider.tsx
@@ -1,13 +1,16 @@
 'use client';
-import { useRef, useEffect } from 'react';
-import { register } from 'swiper/element/bundle';
+import { useState } from 'react';
+import { Autoplay, Pagination } from 'swiper/modules';
+import 'swiper/css/pagination';
+import 'swiper/css';
+import 'swiper/css/bundle';
+import './swiper.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import img1 from './Img1.png';
 import img2 from './Img2.png';
 import Image, { StaticImageData } from 'next/image';
-import styles from './ImageSlider.module.css';
 import Link from 'next/link';
-
-register();
+import styles from './ImageSlider.module.css';
 
 type ImageData = {
    id: number;
@@ -40,80 +43,70 @@ const data: ImageData[] = [
       name: 'Diamond jewelry',
       price: '$69,00',
       img: img2
+   },
+   {
+      id: 5,
+      name: 'Gold big hoops',
+      price: '$58,00',
+      img: img1
+   },
+   {
+      id: 6,
+      name: 'Diamond jewelry',
+      price: '$69,00',
+      img: img2
    }
 ];
 
 export default function ImageSlider() {
-   const swiperElRef = useRef<any>(null);
-   const containerRef = useRef<HTMLDivElement>(null);
+   const extraModules = [Pagination, Autoplay]; // добавляем к слайдеру пагинацию и Autoplay
+   const allowTouchMove = true;
 
-   useEffect(() => {
-      const handleInit = () => {
-         if (containerRef.current) {
-            containerRef.current.classList.add(styles.initiated);
-         }
-      };
+   const pagination = {
+      clickable: true,
 
-      if (swiperElRef.current) {
-         swiperElRef.current.addEventListener('progress', (e: any) => {
-            const [swiper, progress] = e.detail;
-            console.log(progress);
-         });
-
-         swiperElRef.current.addEventListener('slidechange', (e: any) => {
-            console.log('slide changed');
-         });
-
-         swiperElRef.current.addEventListener('init', handleInit);
-
-         // Ensure Swiper is initialized
-         if (swiperElRef.current.swiper) {
-            handleInit();
-         }
+      renderBullet: function (index: number, className: string) {
+         return `<span class=${className}></span>`;
       }
-   }, []);
+   };
+
+   const [, setInit] = useState(false);
 
    return (
-      <div className={styles['swiper-wrapper']} ref={containerRef}>
-         <swiper-container
-            ref={swiperElRef}
-            slides-per-view="1"
-            autoPlay="true"
-            pagination='{
-               "clickable": true
-            }'
-            style={{
-               '--swiper-pagination-color': '#FFF',
-               '--swiper-pagination-bullet-inactive-color': '#a18a68',
-               '--swiper-pagination-bullet-inactive-opacity': '1',
-               '--swiper-pagination-bullet-size': '12px',
-               '--swiper-pagination-bullet-horizontal-gap': '5px'
+      <>
+         <Swiper
+            className={styles['swiper-wrapper']}
+            allowTouchMove={allowTouchMove}
+            modules={extraModules}
+            pagination={pagination}
+            onInit={() => setInit(true)}
+            slidesPerView={1}
+            autoplay={{
+               delay: 4000
             }}
          >
-            {data.map((el) => {
-               return (
-                  <swiper-slide key={el.id}>
-                     <div className={styles['slide-image']}>
-                        <div className={styles['slide-info']}>
-                           <p className={styles['slide-text']}>{el.name}</p>
-                           <span>{el.price}</span>
-                           <Link href={'/shop'}>
-                              <button className={styles['slide-button']}>
-                                 WATCH
-                              </button>
-                           </Link>
-                        </div>
-                        <Image
-                           src={el.img}
-                           alt={el.name + 'image'}
-                           className={styles.img}
-                           priority={true}
-                        />
+            {data.map((el) => (
+               <SwiperSlide key={el.id}>
+                  <div className={styles['slide-image']}>
+                     <div className={styles['slide-info']}>
+                        <p className={styles['slide-text']}>{el.name}</p>
+                        <span>{el.price}</span>
+                        <Link href={'/shop'}>
+                           <button className={styles['slide-button']}>
+                              WATCH
+                           </button>
+                        </Link>
                      </div>
-                  </swiper-slide>
-               );
-            })}
-         </swiper-container>
-      </div>
+                     <Image
+                        src={el.img}
+                        alt={el.name + 'image'}
+                        className={styles.img}
+                        priority={true}
+                     />
+                  </div>
+               </SwiperSlide>
+            ))}
+         </Swiper>
+      </>
    );
 }
